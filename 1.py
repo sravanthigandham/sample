f0b7dfb5499bc2a9792252e284e91139ecd352d7
sravs <sravs@gmail.com>
2023-04-03 14:05:24 +0530

    third

---
+++ 1.py
@@ -0,0 +1,13 @@
+a=24*60
+def xyz(n,m):
+    n = int(input())
+    m = int(input())
+    c=a*n
+    d=a*m
+    print(c)
+    print("for"+str(n)+"days"+str(c)+"minutes")
+    print("for" + str(m) + "days" + str(d) + "minutes")
+    print(c)
+    print(d)
+xyz(n,m)
+print(a)
